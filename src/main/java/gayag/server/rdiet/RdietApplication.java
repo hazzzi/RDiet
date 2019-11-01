0100d6a0b0829685eae70fd1de06eda951a369d0
hanee <limh3030@gmail.com>
2019-11-01 11:49:57 +0900

    Initial commit

---
+++ src/main/java/gayag/server/rdiet/RdietApplication.java
@@ -0,0 +1,13 @@
+package gayag.server.rdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdietApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdietApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gayag/server/rdiet/RdietApplicationTests.java
@@ -0,0 +1,13 @@
+package gayag.server.rdiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RdietApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
